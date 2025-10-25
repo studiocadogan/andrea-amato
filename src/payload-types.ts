70e099bc283ce41268d40cf1facf38c52e525354
Antony Cadogan-White <25158955+studiocadogan@users.noreply.github.com>
2025-10-25 14:50:47 +0100

    bump to v4

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -184,7 +184,7 @@ export interface Page {
             /**
              * Choose how the link should be rendered.
              */
-            appearance?: ('default' | 'outline') | null;
+            appearance?: ('default' | 'outline-solid') | null;
           };
           id?: string | null;
         }[]
@@ -431,7 +431,7 @@ export interface CallToActionBlock {
           /**
            * Choose how the link should be rendered.
            */
-          appearance?: ('default' | 'outline') | null;
+          appearance?: ('default' | 'outline-solid') | null;
         };
         id?: string | null;
       }[]
@@ -481,7 +481,7 @@ export interface ContentBlock {
           /**
            * Choose how the link should be rendered.
            */
-          appearance?: ('default' | 'outline') | null;
+          appearance?: ('default' | 'outline-solid') | null;
         };
         id?: string | null;
       }[]
